3143da1dadde749fa647e79ec31a1c23d9621dbe
Vincent Celis <vincent.celis@gmail.com>
2022-11-25 00:22:50 -0600

    Completed grains.py

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,6 +1,18 @@
-def square(number):
-    pass
+""" Functions for the grains exercism challange"""
 
+
+def square(number):
+    """Calculate how many grains were on a given square."""
+    if not 0 < number < 65:
+        raise ValueError("square must be between 1 and 64")
+    result = 1
+    for _ in range(1, number):
+        result += result
+    return result
 
 def total():
-    pass
+    """Calculate the total number of grains on the chessboard."""
+    result = 0
+    for x in range(1, 65):
+        result += square(x)
+    return result
