ae66dea3cfa56bd6202b47e2fedf8c7fe3383089
Vincent Celis <vincent.celis@gmail.com>
2022-11-25 00:56:58 -0600

    Completed armstrong_numbers

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,14 @@
+"""Function for the Armstrong number exercism challange"""
+
+
 def is_armstrong_number(number):
-    pass
+    """Function to check if given number is Armstrong number.
+
+    An Armstrong number is a number that is the sum of its own
+    digits each raised to the power of the number of digits.
+    """
+    nums = [int(n) for n in str(number)]
+    result = 0
+    for num in nums:
+        result += num**len(nums)
+    return result == number
